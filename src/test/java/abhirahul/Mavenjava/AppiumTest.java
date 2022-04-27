5f0edc35958f82bf1a38aedafacdb8ee4b3736f4
AbhiUv <abhilasha20294@gmail.com>
2022-04-27 14:01:01 +0530

    first commit

---
+++ src/test/java/abhirahul/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package abhirahul.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void postjira() 
+	{
+		System.out.println("postjira");
+	}
+	@Test
+	public void deletetwitter() 
+	{
+		System.out.println("deletetwitter");
+	}
+
+}
---
+++ src/test/java/abhirahul/Mavenjava/RESTAPITest.java
@@ -0,0 +1,19 @@
+package abhirahul.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void motive() 
+	{
+		System.out.println("motive");
+	}
+	@Test
+	public void base() 
+	{
+		System.out.println("base");
+	}
+
+
+}
